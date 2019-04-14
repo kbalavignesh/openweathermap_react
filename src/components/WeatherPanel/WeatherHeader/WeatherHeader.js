fa2f1c9ae8ad176c9ea18458dc4c0dbe42418b5f
Arthidevi <ab237@hw.ac.uk>
2019-04-15 00:22:41 +0100

    Jumbotran margin corrected

--- src/components/WeatherPanel/WeatherHeader/WeatherHeader.js
+++ src/components/WeatherPanel/WeatherHeader/WeatherHeader.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import edinImage from '../../../assets/images/edinburgh.jpg';
 
-const weatherHeader = (props) => (
-  <header className="jumbotron my-4">
+class weatherHeader extends Component {
+  paddingStyle = {
+                  padding: '1rem 1rem'
+              };
+
+  render() {
+  return (<header className="jumbotron my-4" style={this.paddingStyle}>
   <div className="row">
     <div className="col-lg-8 col-md-8 col-sm-8 col-5 mb-4">
-        <h1 className="display-6">Open Weather Map - Edinburgh,UK</h1>
-        <a href="#" onClick={props.loadMethod}className="btn btn-primary btn-lg">Reload Weather</a>
+        <h2 className="display-6">Weather Forecast - Edinburgh,UK</h2>
+        <a href="#" onClick={this.props.loadMethod}className="btn btn-primary btn-lg">Update Weather</a>
     </div>
     <div className="col-lg-4 col-md-4 col-sm-4 col-5 mb-4">
       <img src={edinImage} class="img-thumbnail" alt="Edinburgh City" width="304" height="236"/>
     </div>
   </div>
-  </header>
-);
+  </header>);
+}
+}
 
 export default weatherHeader;
